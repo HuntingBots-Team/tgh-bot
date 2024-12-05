373eb369f325cc5aaabef296af728f90576da717
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2024-12-05 17:58:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# Run update.py and then tghbot
-python3 update.py && python3 -m tghbot
+# Check if directories exist, if not, create them
+[ ! -d "plugins" ] && mkdir plugins
+[ ! -d "sabnzbdapi" ] && mkdir sabnzbdapi
+[ ! -d "tghbot" ] && mkdir tghbot
+
+# Activate the virtual environment
+source botenv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Start the bot
+python tghbot/update.py
