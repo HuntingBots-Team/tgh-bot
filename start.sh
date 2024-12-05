dbc690c2ba13ebefa2076ee99c419043992caa9b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2024-12-06 02:55:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-
-# Check if directories exist, if not, create them
-[ ! -d "myjd" ] && mkdir myjd
-[ ! -d "py_generators" ] && mkdir py_generators
-[ ! -d "qBittorrent/config" ] && mkdir qBittorrent/config
-[ ! -d "sabnzbd" ] && mkdir sabnzbd
-[ ! -d "sabnzbdapi" ] && mkdir sabnzbdapi
-[ ! -d "tghbot" ] && mkdir tghbot
-[ ! -d "web" ] && mkdir web
-
-# Activate the virtual environment
 source botenv/bin/activate
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Start the bot
 python3 update.py
 python3 -m tghbot
