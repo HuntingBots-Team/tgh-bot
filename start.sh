edcbd902395df1e2745e0ccc969617183f9b20dd
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2024-12-09 11:02:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source /usr/src/app/botenv/bin/activate
-
-# Source the environment variables from config.env
-if [ -f /usr/src/app/config.env ]; then
-source /usr/src/app/config.env
-else
-echo "config.env file not found!"
-exit 1
-fi
-
-# Run the update script
-python3 /usr/src/app/update.py
-
-# Run the tghbot module
+source botenv/bin/activate
+python3 update.py
 python3 -m tghbot
