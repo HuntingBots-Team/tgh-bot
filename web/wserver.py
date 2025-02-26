32e4bf1261bc4355b10d42cd872626767bfb921b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2025-02-26 13:12:44 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -66,7 +66,7 @@ aria2 = ariaAPI(
 basicConfig(
     format="%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s",
     handlers=[
-        FileHandler("Zee_Logs.txt"),
+        FileHandler("TGH_Logs.txt"),
         StreamHandler()
     ],
     level=INFO,
