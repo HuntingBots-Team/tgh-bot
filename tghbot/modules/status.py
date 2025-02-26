bbbf6d36d34f8d0bffa688cd2f98515844dbf6c4
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2025-02-26 13:47:09 +0530

    Update status.py

--- tghbot/modules/status.py
+++ tghbot/modules/status.py
@@ -260,7 +260,7 @@ def bot_sys_stats():
     bmsg += f"S: {swap}% | "
     bmsg += f"D: {disk}%\n\n"
     bmsg += f"Bandwidth Used: {traf}\n"
-    bmsg += f"{def_media(BASE.encode()).decode()}"
+    bmsg += f"Powered by HuntingBots"
     return bmsg
 
 
@@ -291,7 +291,7 @@ async def stats(_, message, edit_mode=False):
     mem_p = memory.percent
     swap = swap_memory()
 
-    bot_stats = f"<b><i><u>Zee Bot Statistics</u></i></b>\n\n"\
+    bot_stats = f"<b><i><u>TGHThingLeech_bot Bot Statistics</u></i></b>\n\n"\
                 f"<code>CPU  : </code>{get_progress_bar_string(cpuUsage)} {cpuUsage}%\n" \
                 f"<code>RAM  : </code>{get_progress_bar_string(mem_p)} {mem_p}%\n" \
                 f"<code>SWAP : </code>{get_progress_bar_string(swap.percent)} {swap.percent}%\n" \
@@ -301,7 +301,7 @@ async def stats(_, message, edit_mode=False):
                 f"<code>Downloaded      : </code> {recv}\n" \
                 f"<code>Total Bandwidth : </code> {tb}"
 
-    sys_stats = f"<b><i><u>Zee System Statistics</u></i></b>\n\n"\
+    sys_stats = f"<b><i><u>TGHThingLeech_bot System Statistics</u></i></b>\n\n"\
                 f"<b>System Uptime:</b> <code>{sysTime}</code>\n" \
                 f"<b>CPU:</b> {get_progress_bar_string(cpuUsage)}<code> {cpuUsage}%</code>\n" \
                 f"<b>CPU Total Core(s):</b> <code>{cpu_count(logical=True)}</code>\n" \
@@ -486,7 +486,7 @@ async def send_repo_stats(_, query):
             if version != vtag:
                 update_info =  f"⚠️ New Version Update Available ⚠️"
 
-    repo_stats = f"<b><i><u>Zee Repository Info</u></i></b> \n\n" \
+    repo_stats = f"<b><i><u>TGHThingLeech_bot Repository Info</u></i></b> \n\n" \
                  f"<b><i>Official Repository</i></b>        \n"   \
                  f"<code>- Updated   : </code> {commit_date}\n"   \
                  f"<code>- Version   : </code> {vtag}       \n"   \
@@ -539,7 +539,7 @@ async def send_bot_limits(_, query):
     UMT = "Unlimited" if config_dict["USER_MAX_TASKS"] == "" else config_dict["USER_MAX_TASKS"]
     BMT = "Unlimited" if config_dict["QUEUE_ALL"] == "" else config_dict["QUEUE_ALL"]
 
-    bot_limit = f"<b><i><u>Zee Bot Limitations</u></i></b>\n" \
+    bot_limit = f"<b><i><u>TGHThingLeech_bot Bot Limitations</u></i></b>\n" \
                 f"<code>Torrent   : {TOR}</code> <b>GB</b>\n" \
                 f"<code>G-Drive   : {GDL}</code> <b>GB</b>\n" \
                 f"<code>Yt-Dlp    : {YTD}</code> <b>GB</b>\n" \
@@ -659,4 +659,4 @@ bot.add_handler( # type: ignore
             "^status"
         )
     )
-)
+    )
